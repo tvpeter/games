7c27d5718426118d86ecb5c51b5213d22d50e4f1
Vihiga Tyonum <withtvpeter@gmail.com>
2023-11-15 10:33:20 +0100

    add critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,26 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react"
 
 import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList"
+import CriticScore from "./CriticScore"
 
 interface GameProps {
   game: Game
 }
 const GameCard = ({ game }: GameProps) => {
-  
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((platform) => platform.platform)}
-        />
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
+            platforms={game.parent_platforms.map(
+              (platform) => platform.platform
+            )}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   )
