c168925a86317426bf824bd5f482aac72cf2b155
Vihiga Tyonum <withtvpeter@gmail.com>
2023-11-16 18:38:44 +0100

    add filtering games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 
 interface Response<T> {
     count: number;
     results: T[];
 }
-const useData = <T>(endpoint: string) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([])
     const [error, setError] = useState("")
     const [isLoading, setLoading] = useState(false);
@@ -17,7 +18,7 @@ const useData = <T>(endpoint: string) => {
 
         setLoading(true);
         apiClient
-            .get<Response<T>>(endpoint, { signal: controller.signal })
+            .get<Response<T>>(endpoint, { signal: controller.signal, ...requestConfig })
             .then((res) => {
                 setData(res.data.results)
                 setLoading(false);
@@ -29,7 +30,7 @@ const useData = <T>(endpoint: string) => {
             })
         
         return () => controller.abort();
-    }, [])
+    }, deps? [...deps] : [])
 
     return { data, error, isLoading };
 }
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
     id: number;
@@ -48,6 +49,10 @@ const useGames = () => {
 }
 */
 
-const useGames = () => useData<Game>('/games');
+const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', {
+    params: {
+        genres: selectedGenre?.id
+    }
+}, [selectedGenre?.id]);
 
 export default useGames;
