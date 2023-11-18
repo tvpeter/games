810219028e37a8cb8f76543027bce8e301bb0c98
Vihiga Tyonum <withtvpeter@gmail.com>
2023-11-18 14:07:17 +0100

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
+import noimage from '../assets/no-image-placeholder-6f3882e0.webp';
 
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+    if (!url) return noimage;
     const target = 'media/';
 
     const index = url.indexOf(target) + target.length;
