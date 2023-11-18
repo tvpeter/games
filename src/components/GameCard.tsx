ae66d86c31c6255eb5b77268282c2c33a797e786
Vihiga Tyonum <withtvpeter@gmail.com>
2023-11-18 16:08:19 +0100

    cleanup game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,10 +11,9 @@ interface GameProps {
 const GameCard = ({ game }: GameProps) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={ getCroppedImageUrl(game.background_image)} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="lg" >{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map(
               (platform) => platform.platform
@@ -22,6 +21,7 @@ const GameCard = ({ game }: GameProps) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="lg">{game.name}</Heading>
       </CardBody>
     </Card>
   )
